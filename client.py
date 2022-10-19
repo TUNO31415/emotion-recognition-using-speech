5d3dd2e490a7b3f338fbc2a2011d844fa0ed128e
Taichi Uno <T.Uno@student.tudelft.nl>
2022-10-20 01:50:58 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,37 +1,21 @@
-from crypt import methods
-from urllib import request
-from flask import Flask, jsonify, request
-import pandas as pd
+from flask import Flask, jsonify
 from emotion_recognition import EmotionRecognizer
-from sklearn.svm import SVC
-import datetime
+from sklearn.neural_network import MLPClassifier
 
 app = Flask(__name__)
-rec = EmotionRecognizer(model=SVC(), emotions=['sad', 'neutral', 'happy'], balance=True, verbose=0)
+rec = EmotionRecognizer(model=MLPClassifier(alpha=1, max_iter=1000), emotions=['sad', 'neutral', 'happy'], balance=True, verbose=0)
 rec.train()
 
-states =[
-    {'date' : '01/11/2022', 'time' : '11:00:19', 'state' : 'happy'}
-]
-
-@app.route('/audio')
-def hello_world():
-    return jsonify(states)
+# For test purpose
+@app.route('/audio', methods=['GET'])
+def get_state():
+    prediction = rec.predict_proba("data/emodb/wav/16a01Wb.wav")
+    return jsonify(prediction)
 
 # Get audio somehow from furhat using POST request
 @app.route('/audio', methods=['POST'])
 def get_audio():
-    now = datetime.now()
-    date = now.strftime('%H:%M:%S')
-    time = now.strftime('%d/%m/%Y')
-    prediction = rec.pridict('PATH-TO-AUDIO')
-
-    states = [
-        {'date' : date,
-        'time' : time,
-        'state' : prediction}
-    ]
-    return f'Prediction : {prediction}', 204
+    return jsonify(rec.pridict_proba('PATH-TO-AUDIO'))
 
 if __name__ == '__main__':
     app.run()
